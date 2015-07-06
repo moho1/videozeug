bdf3d0dc2f20432ab410113693250959a7b0e79a
Moritz Holtz <moritz.holtz@rwth-aachen.de>
2015-07-06 08:51:07 +0200

    updated makefile for cornerpin.cpp, added non-ocl mode for cornerpin.cpp

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-cornerpin:
-	g++ -o cornerpin -std=c++14 $$(pkg-config --cflags --libs opencv) -Wall -Ofast -funroll-loops -march=native cornerpin.cpp
+cornerpin: cornerpin.cpp
+	clang++ -o cornerpin -std=c++14 $$(pkg-config --cflags --libs opencv) -Wall -Ofast -funroll-loops -march=native cornerpin.cpp
--- cornerpin.cpp
+++ cornerpin.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <opencv2/opencv.hpp>
-#include <opencv2/ocl/ocl.hpp>
 #include <fstream>
 #include <string>
 #include <opencv/cv.h>
 #include <opencv/ml.h>
 #include <opencv/cxcore.h>
 #include <opencv/highgui.h>
+
+//#define CP_SHOW
+#define CP_OCL
+
+#ifdef CP_OCL
+#include <opencv2/ocl/ocl.hpp>
+#endif
 
 using namespace cv;
 
@@ -31,19 +37,33 @@ double timer()
 }
 
 void cpvid(VideoCapture &invid, VideoWriter &outvid, cv::Mat H) {
+#ifdef CP_OCL
 	cv::Mat cpuframe;
 	cv::ocl::oclMat inframe, outframe, oclH;
-	
 	oclH = H;
+#else
+	cv::Mat inframe, outframe;
+#endif	
 	
 	double starttime = timer();
 	double now;
 	
 	for (int counter = 0; ; counter++) {
+#ifdef CP_OCL
 		if (!invid.read(cpuframe)) break;
 		inframe = cpuframe;
 		cv::ocl::warpPerspective(inframe, outframe, oclH, cv::Size(1920,1080));
-		outvid.write(outframe);
+#else
+		if (!invid.read(inframe)) break;
+		cv::warpPerspective(inframe, outframe, H, cv::Size(1920,1080));
+#endif
+#ifdef CP_SHOW
+		cv::imshow("Input", inframe);
+		cv::imshow("Output", outframe);
+		cv::waitKey(1);
+#endif
+//		outvid.write(outframe);
+//		std::cout << outframe.toString();
 		now = timer();
 		std::cerr 
 			<< (counter+1) << " "
@@ -56,10 +76,14 @@ void cpvid(VideoCapture &invid, VideoWriter &outvid, cv::Mat H) {
 int main(int argc, char **argv) {
 	int fourcc = CV_FOURCC('M','J','P','G');
 	
-	VideoCapture invid("/media/Data/Videos/VideoAG/diff/15ss-dbis-150623/00055.MTS");
+	VideoCapture invid(argv[9]);
 	std::cerr << "reader open? " << (invid.isOpened() ? "yes" : "NO") << std::endl;
+
 	
-	VideoWriter outvid("cpptest.avi", fourcc, 25, cv::Size(1920,1080));
+	VideoWriter outvid(argv[10], 
+		fourcc,
+		25,
+		cv::Size(1920,1080));
 	std::cerr << "writer open? " << (outvid.isOpened() ? "yes" : "NO") << std::endl;
 	
 	
